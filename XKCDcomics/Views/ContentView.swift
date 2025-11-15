3a292faeda6e79da0c5029af340b5d066210b63b
Otto Alexander Robsahm <111810346+Otro1@users.noreply.github.com>
2025-11-15 11:53:08 +0100

    Project setup
    - Create folders
    - Clean up template code
    - Set orientation to portrait
    - Remove Vision and Mac as supported destinations
    - Set minimum deployment to 17.6
    - Set app category to Magazines & Newspapers

---
+++ XKCDcomics/Views/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  XKCDcomics
+//
+//  Created by Otto A Robsahm on 15/11/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+
+    var body: some View {
+    }
+}
